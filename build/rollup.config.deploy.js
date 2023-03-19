6bcadb822e3a30d67e5a2aa7cc5a54ca2693c6ed
Suiko <suiko@riseup.net>
2023-03-19 22:45:45 +0800

    fix(deploy): fix deploy error

--- build/rollup.config.deploy.js
+++ build/rollup.config.deploy.js
@@ -1,11 +1,11 @@
 process.env.NODE_ENV = "production";
-const { uglify } = require("rollup-plugin-uglify");
 const configList = require("./rollup.config");
+const terser = require("@rollup/plugin-terser");
 
 configList.map((config, index) => {
   config.output.sourcemap = false;
   config.output.file = "example/index.js";
-  config.plugins = [...config.plugins, ...[uglify()]];
+  config.plugins = [...config.plugins, [terser()]];
 
   return config;
 });
