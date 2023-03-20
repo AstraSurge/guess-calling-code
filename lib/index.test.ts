702512a47969a55d6b38eb90618d7c6419600d27
Suiko <suiko@riseup.net>
2023-03-20 23:32:47 +0800

    test: update test to fit node env without Intl module

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,9 +1,17 @@
 import guessCallingCode from ".";
 
 describe("guessCallingCode", () => {
-  it("should return a string", () => {
-    expect(typeof guessCallingCode()).toBe("string");
+  it("should return 1 when timezone is America/New_York", () => {
+    global.Intl = {
+      DateTimeFormat: () => ({
+        resolvedOptions: () => ({
+          timeZone: "America/New_York",
+        }),
+      }),
+    } as typeof Intl;
+    expect(guessCallingCode()).toBe("1");
   });
+
   it("should return undefined if Intl is not available", () => {
     global.Intl = undefined;
     expect(guessCallingCode()).toBe(undefined);
